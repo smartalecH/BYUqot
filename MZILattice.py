930449b3e1641d1960ca07d0f70d27328d37da0a
Alec Hammond <alec.m.hammond@gmail.com>
2018-04-09 13:49:50 -0600

    Added top leg

--- MZILattice.py
+++ MZILattice.py
@@ -6,38 +6,46 @@ import gdspy
 #      Create single MZI
 # ------------------------------------------------------------------ #
 
+def MZI(deltaL = 20, Lref = 20, gapLength = 10, waveguideWidth = 0.5, bendRadius = 5):
 
-waveguideWidth = 0.5
+    LTop = Lref - deltaL
 
-L = 20
-LTop = 30
+    leg = (LTop - Lref) / 2
 
-legs = (LTop - L) / 2
+    YBranch = obLib.YBranch()
 
-YBranch = obLib.YBranch()
+    # Connect bottom branch
+    bottomBranch = gdspy.Cell('referenceBranch')
+    bottomBranch.add(gdspy.Rectangle([-Lref/2 , waveguideWidth/2],[Lref/2, -waveguideWidth/2],layer=1))
 
-# Connect bottom branch
-bottomBranch = gdspy.Cell('outputBranch')
-bottomBranch.add(gdspy.Rectangle([-L/2 , waveguideWidth/2],[L/2, -waveguideWidth/2],layer=1))
+    # Do top branch
+    outerArm = (Lref-gapLength)/2
+    print(outerArm)
+    length = [outerArm,leg,gapLength,leg,outerArm]
+    turn = [-1,1,1,-1]
+    topBranch = gdspy.Cell('deltaBranch')
+    topBranchPoly = (0, 0), '+x', waveguideWidth, length, turn,layer=1);
+    topBranch.add(gdspy.L1Path(topBranchPoly)
 
-# Do top branch
 
+    # Half the hieght of the entire splitter - used to align branches with splitters
+    cen2Branch = 2.0 + 1.2/2 - waveguideWidth/2.0
 
+    MZIcell = gdspy.Cell('MZI')
+    MZIcell.add(gdspy.CellReference(YBranch, (-Lref/2 - 15,0)))
+    MZIcell.add(gdspy.CellReference(YBranch, (Lref/2 + 15,0),rotation=180))
+    MZIcell.add(gdspy.CellReference(bottomBranch, (0,-(cen2Branch))))
+    MZIcell.add(gdspy.CellReference(topBranch, (-Lref/2,+(cen2Branch))))
 
-
-
-cen2Branch = 2 + 1.2/2 - 5/2.0
-
-MZI = gdspy.Cell('MZI')
-MZI.add(gdspy.CellReference(YBranch, (-L/2 - 15,0)))
-MZI.add(gdspy.CellReference(YBranch, (L/2 + 15,0),rotation=180))
-MZI.add(gdspy.CellReference(bottomBranch, (0,-(cen2Branch))))
+    return MZIcell
 
 
 
 # ------------------------------------------------------------------ #
 #      OUTPUT
 # ------------------------------------------------------------------ #
+
+MZIcell = MZI(deltaL = 10, Lref = 20, waveguideWidth = 0.5);
 
 # Output the layout to a GDSII file (default to all created cells).
 # Set the units we used to micrometers and the precision to nanometers.
