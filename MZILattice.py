c580302d12bb5cbda035bf9187fd61e77e6087f0
Alec Hammond <alec.m.hammond@gmail.com>
2018-04-10 09:09:47 -0600

    Fixed upper leg bug

--- MZILattice.py
+++ MZILattice.py
@@ -6,9 +6,11 @@ import gdspy
 #      Create single MZI
 # ------------------------------------------------------------------ #
 
-def MZI(deltaL = 20, Lref = 20, gapLength = 10, waveguideWidth = 0.5, bendRadius = 5):
+def MZI(deltaL = 40, Lref = 20, gapLength = 10, waveguideWidth = 0.5, bendRadius = 5):
 
-    LTop = Lref - deltaL
+    layerNumber = 1
+
+    LTop = Lref + deltaL
 
     leg = (LTop - Lref) / 2
 
@@ -19,13 +21,23 @@ def MZI(deltaL = 20, Lref = 20, gapLength = 10, waveguideWidth = 0.5, bendRadius
     bottomBranch.add(gdspy.Rectangle([-Lref/2 , waveguideWidth/2],[Lref/2, -waveguideWidth/2],layer=1))
 
     # Do top branch
+    bendRadius = 5;
+
     outerArm = (Lref-gapLength)/2
-    print(outerArm)
+
     length = [outerArm,leg,gapLength,leg,outerArm]
-    turn = [-1,1,1,-1]
+    print(length)
+    turn = [1,-1,-1,1]
     topBranch = gdspy.Cell('deltaBranch')
-    topBranchPoly = (0, 0), '+x', waveguideWidth, length, turn,layer=1);
-    topBranch.add(gdspy.L1Path(topBranchPoly)
+
+    topBranchPoly = gdspy.L1Path((0, 0), '+x', waveguideWidth, length, turn,layer=1);
+    topBranchPoly.fillet(bendRadius)
+    leftSquare    = gdspy.Rectangle([0,-waveguideWidth/2],[waveguideWidth,waveguideWidth/2],layer=1)
+    rightSquare   = gdspy.Rectangle([Lref-waveguideWidth,-waveguideWidth/2],[Lref,waveguideWidth/2],layer=1)
+
+    union = gdspy.fast_boolean(topBranchPoly,[leftSquare,rightSquare],'or',layer=layerNumber)
+
+    topBranch.add(union)
 
 
     # Half the hieght of the entire splitter - used to align branches with splitters
@@ -45,7 +57,7 @@ def MZI(deltaL = 20, Lref = 20, gapLength = 10, waveguideWidth = 0.5, bendRadius
 #      OUTPUT
 # ------------------------------------------------------------------ #
 
-MZIcell = MZI(deltaL = 10, Lref = 20, waveguideWidth = 0.5);
+MZIcell = MZI(deltaL = 30, Lref = 20, waveguideWidth = 0.5);
 
 # Output the layout to a GDSII file (default to all created cells).
 # Set the units we used to micrometers and the precision to nanometers.
