2a941eb0f5d824bffc23aed48ba504b55213ee20
Alec Hammond <alec.m.hammond@gmail.com>
2018-05-18 10:19:35 -0600

    Added tutorials

--- meep/mrr.py
+++ meep/mrr.py
@@ -33,7 +33,7 @@ Si   = mp.Medium(epsilon=12)
 SiO2 = mp.Medium(epsilon=3)
 
 # All units in microns
-ringRadius        = 10      # radius of the center of the ring waveguide
+ringRadius        = 5      # radius of the center of the ring waveguide
 ringWaveguideWith = 0.5     # width of the ring waveguide
 gapLength         = 0.2     # distance between ring edge and bus
 topBusWidth       = 0.5     # width of top bus
@@ -42,8 +42,8 @@ topBus            = True    # Boolean - true if including top bus
 bottomBus         = True    # Boolean - true if including bottom bus
 racetrackLength   = 0       # length of the racetrack
 
-waveguideThickness = 0.22   # thickness of all waveguides
-
+waveguideThickness = 0   # thickness of all waveguides
+sim2D = mp.inf
 # ------------------------------------------------------------------ #
 # Draw geometry
 # ------------------------------------------------------------------ #
@@ -54,13 +54,13 @@ geometry = [];
 # draw the ring
 outerRingGeom = mp.Cylinder(
                     radius = ringRadius + ringWaveguideWith / 2,
-                    height = waveguideThickness,
+                    height = sim2D,
                     center = mp.Vector3(0,0,0),
                     material = Si
                     )
 innerRingGeom = mp.Cylinder(
                     radius = ringRadius - ringWaveguideWith / 2,
-                    height = waveguideThickness,
+                    height = sim2D,
                     center = mp.Vector3(0,0,0),
                     material = SiO2
                     )
@@ -71,7 +71,7 @@ geometry.append(innerRingGeom)
 if topBus:
     yPos = ringRadius + ringWaveguideWith/2 + gapLength + topBusWidth/2;
     topBusGeom = mp.Block(
-       mp.Vector3(2*ringRadius + ringWaveguideWith,bottomBusWidth,waveguideThickness),
+       mp.Vector3(3*ringRadius,bottomBusWidth,sim2D),
        center = mp.Vector3(0,yPos,0),
        material = Si
        )
@@ -82,7 +82,7 @@ if topBus:
 if bottomBus:
     yPos = ringRadius + ringWaveguideWith/2 + gapLength + bottomBusWidth/2;
     bottomBusGeom = mp.Block(
-       mp.Vector3(2*ringRadius + ringWaveguideWith,bottomBusWidth,waveguideThickness),
+       mp.Vector3(3*ringRadius,bottomBusWidth,sim2D),
        center = mp.Vector3(0,-yPos,0),
        material = Si
        )
@@ -93,15 +93,24 @@ if bottomBus:
 # ------------------------------------------------------------------ #
 
 # Size of domain
-cell = mp.Vector3(2*ringRadius + ringWaveguideWith,2*ringRadius + ringWaveguideWith,3*waveguideThickness)
+bufferY = 2
+cellX = 3*ringRadius
+cellY = 2*(ringRadius + ringWaveguideWith/2 + gapLength + ringWaveguideWith + bufferY)
+cellZ = 0
+cell = mp.Vector3(cellX,cellY,cellZ)
 
 # Sources
-sources = [mp.Source(mp.ContinuousSource(frequency=0.15),
-            component=mp.Ez,
-            center=mp.Vector3(0,0))]
+
+cY = -(ringRadius + ringWaveguideWith + gapLength)
+sources = [ mp.EigenModeSource(src=mp.ContinuousSource(wavelength=1.55),
+                               eig_band=1,
+                               size=mp.Vector3(0,3,0),
+                               center=mp.Vector3(-ringRadius,cY,0),
+                               eig_parity=mp.ODD_Z
+                               ) ]
 
 # Boundary conditions
-pml_layers = [mp.PML(1.0)]
+pml_layers = [mp.PML(2)]
 
 # Resolution
 resolution = 10
@@ -110,11 +119,25 @@ sim = mp.Simulation(cell_size = cell,
                     boundary_layers = pml_layers,
                     geometry = geometry,
                     sources = sources,
-                    resolution = resolution)
-
+                    resolution = resolution,
+                    default_material=SiO2,
+                    force_complex_fields=True)
+sim.run(until=200)
 eps_data = sim.get_array(center=mp.Vector3(),size=cell,component=mp.Dielectric)
+cellSize = eps_data.shape
+cellSizeX = cellSize[0]
+cellSizeY = cellSize[1]
+#cellSizeZ = cellSize[2]
+#eps_data_mod = np.squeeze(eps_data[:,:,round(cellSizeZ/2)])
 
-
+ez_data = sim.get_array(center=mp.Vector3(),size=cell,component=mp.Ez)
+#ez_data_mod = np.squeeze(ez_data[:,:,round(cellSizeZ/2)])
 # ------------------------------------------------------------------ #
 # Visualize geometry
 # ------------------------------------------------------------------ #
+
+plt.figure(dpi=150)
+plt.imshow(eps_data.transpose(),interpolation='spline36',cmap='binary')
+plt.imshow(np.real(ez_data.transpose()),interpolation='spline36',cmap='RdBu', alpha=0.9)
+plt.axis('off')
+plt.show()
